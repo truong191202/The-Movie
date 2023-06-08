f56420853e277020025ff9d7221499d9dd5f7f4a
Vu Nguyen Truong <vunguyentruong2002@gmail.com>
2023-06-08 18:12:14 +0700

    test

--- components/Pages/home/Join.tsx
+++ components/Pages/home/Join.tsx
@@ -11,17 +11,21 @@ function Join() {
 					<p className="mt-5 text-lg font-thin">
 						Get access to maintain your own{" "}
 						<span className="opacity-70 italic">
-							custom personal lists, track what you've seen
+							custom personal lists, track what you&apos;ve seen
 						</span>{" "}
 						and search and filter for{" "}
 						<span className="opacity-70 italic">
 							what to watch next
 						</span>
-						—regardless if it's in theatres, on TV or available on
-						popular streaming services like Netflix, Amazon Prime
+						—regardless if it&apos;s in theatres, on TV or available
+						on popular streaming services like Netflix, Amazon Prime
 						Video, Disney Plus, Apple TV Plus, and fuboTV.
 					</p>
-					<Link href={'./signup'}><button className="text-white px-5 py-2 rounded-md mt-6 font-bold bg-accountPurple">Sign Up</button></Link>
+					<Link href={"./signup"}>
+						<button className="text-white px-5 py-2 rounded-md mt-6 font-bold bg-accountPurple">
+							Sign Up
+						</button>
+					</Link>
 				</div>
 				<div className="text-sm opacity-80 ml-16 mt-14">
 					<ul className="list-disc">
@@ -31,7 +35,7 @@ function Join() {
 							Filter by your subscribed streaming services and
 							find something to watch
 						</li>
-						<li>Log the movies and TV shows you've seen</li>
+						<li>Log the movies and TV shows you&apos;ve seen</li>
 						<li>Build custom lists</li>
 						<li>Contribute to and improve our database</li>
 					</ul>
